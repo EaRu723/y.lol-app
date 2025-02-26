ab822927076d75f0de84caa51a2c0b5935adf3a4
EaRu723 <y.andrearusso@gmail.com>
2025-02-25 19:16:51 -0500

    init commit

---
+++ y.lol/y_lolApp.swift
@@ -0,0 +1,17 @@
+//
+//  y_lolApp.swift
+//  y.lol
+//
+//  Created by Andrea Russo on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct y_lolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
